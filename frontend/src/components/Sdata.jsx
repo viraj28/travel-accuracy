964749aee5f6f6825b65299f1465693bc8e5196b
Viraj Nirbhavane <vnirbhavane28@gmail.com>
2023-01-08 16:25:42 +0530

    Update service data

--- frontend/src/components/Sdata.jsx
+++ frontend/src/components/Sdata.jsx
@@ -4,19 +4,19 @@ import safeguard from '../images/safeguard.jpg';
 
 const Sdata = [
     {
-        imgsrc: network,
-        title: 'DMC Connections',
-        desc: 'We have a network of DMCs and are here to help you connect with the right DMC for you.',
-    },
-    {
         imgsrc: happy,
-        title: 'Happy Customers',
-        desc: 'Good Trips equals to happy customers',
+        title: 'Our Commitment',
+        desc: 'We are determined to provide a top-quality experience for the customers by building a bridge between the Indian tour operators and a network of world-renowned international DMC’s. Our ultimate goal is customer satisfaction and to make sure we achieve that by creating new benchmarks.  We aim to evolve the stereotypes in travel industry and to ensure that no stone is left unturned to guarantee a unique experience.',
     },
     {
         imgsrc: safeguard,
         title: 'Money Safety',
-        desc: 'Ensuring your customers satisfaction is our priority',
+        desc: 'We aim to achieve and uphold highest form of ethical standards. Our top priority is to eliminate the threat of unsecured payments to foreign non established DMC’s risking the integrity and reputation of emerging travel agency.  We guarantee to safeguard your interest and ensure a trusted way to make payments for a superior and safe experience ',
+    },
+    {
+        imgsrc: network,
+        title: 'DMC Connections',
+        desc: 'Our agenda is to build a relationship between the Travel Agent with the most reputed and renowned DMC’s in the world. This connection is made with the motivation to ease the difficulties of finding the perfect partner to collaborate with in the travel industry. We are passionate about curating a distinctive travel package in the most seamless way possible. Our vision is to provide and deliver multiple value-added services to our partners with the purpose of ensuring customer satisfaction.',
     },
 ];
 
